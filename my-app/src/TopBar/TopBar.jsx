76034125a39b7d3497083121da860883cf4aa953
Jamil Weeks <jamilweeks@yahoo.com>
2022-05-03 22:30:45 -0400

    initial push

---
+++ my-app/src/TopBar/TopBar.jsx
@@ -0,0 +1,26 @@
+import "./TopBar.css"
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topIcon fa-brands fa-facebook-square"></i>
+                <i className="topIcon fa-brands fa-twitter-square"></i>
+                <i className="topIcon fa-brands fa-instagram-square"></i>
+                <i className="topIcon fa-brands fa-youtube-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                   <li className="topListItem">HOME</li> 
+                   <li className="topListItem">ABOUT</li> 
+                   <li className="topListItem">CONTACT</li> 
+                   <li className="topListItem">WRITE</li> 
+                   <li className="topListItem">LOGOUT</li> 
+                </ul>
+            </div>
+            <div className="topRight">
+                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i> 
+            </div>
+        </div>
+    )
+}
