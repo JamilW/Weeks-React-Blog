8b70c6ffc2002303ff48541174d5edc4d9ffc861
Jamil Weeks <jamilweeks@yahoo.com>
2022-05-17 19:34:46 -0400

    minor adjustments

--- my-app/src/header/header.jsx
+++ my-app/src/header/header.jsx
@@ -4,8 +4,8 @@ export default function header() {
     return  (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitlesSm">React & Node</span>
-                <span className="headerTitlesLg">Blog</span>
+                <span className="headerTitlesSm">Film | Music | Sports</span>
+                <span className="headerTitlesLg">Weeks Agenda</span>
             </div>  
             <img className="headerImg" src="https://images.pexels.com/photos/1287083/pexels-photo-1287083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
     </div>
