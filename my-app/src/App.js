b99112fe2a2b2c77a2fecac1bb931fc97ebbefe2
Jamil Weeks <jamilweeks@yahoo.com>
2023-02-06 20:13:07 -0500

    created singe page component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
-import Home from "./pages/Home/home";
-import TopBar from "./TopBar/TopBar";
+// import Home from "./pages/Home/home";
+import TopBar from "./components/TopBar/TopBar";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
    <>
     <TopBar/>
-    <Home/>
+    {/* <Home/> */}
+    <Single/>
     </>
   );
 }
