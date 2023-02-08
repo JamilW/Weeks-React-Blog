3e8cd56ffe8c63aaefbfc1331ca3ff5ce6700344
Jamil Weeks <jamilweeks@yahoo.com>
2023-02-08 12:20:18 -0500

    worked on user settings component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 // import Home from "./pages/Home/home";
 import TopBar from "./components/TopBar/TopBar";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write"
+// import Write from "./pages/write/Write"
+import Settings from "./pages/settings/Settings"
+
 
 function App() {
   return (
    <>
-    <TopBar/>
-    {/* <Home/> */}
-    {/* <Single/> */}
-    <Write/>
+    <TopBar />
+    {/* <Home />
+    <Single />
+    <Write /> */}
+    <Settings />
     </>
   );
 }
