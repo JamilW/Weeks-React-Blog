2080f55a8cd9460d63887d5742cb4e5fb22a964c
Jamil Weeks <jamilweeks@yahoo.com>
2023-02-08 10:54:55 -0500

    worked on logic and styling for creating new post

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 // import Home from "./pages/Home/home";
 import TopBar from "./components/TopBar/TopBar";
-import Single from "./pages/single/Single";
+// import Single from "./pages/single/Single";
+import Write from "./pages/write/Write"
 
 function App() {
   return (
    <>
     <TopBar/>
     {/* <Home/> */}
-    <Single/>
+    {/* <Single/> */}
+    <Write/>
     </>
   );
 }
