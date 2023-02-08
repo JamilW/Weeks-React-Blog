3b3cb98fc4f090ea4de9018a97eb213a94269cb0
Jamil Weeks <jamilweeks@yahoo.com>
2023-02-08 16:20:13 -0500

    worked on linking pages stylig and logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,37 @@
-// import Home from "./pages/Home/home";
+import Home from "./pages/Home/home";
 import TopBar from "./components/TopBar/TopBar";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write"
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write"
 import Settings from "./pages/settings/Settings"
-
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom"
 
 function App() {
+  const user = false;
   return (
-   <>
+   <Router>
     <TopBar />
-    {/* <Home />
-    <Single />
-    <Write /> */}
-    <Settings />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+      <Route path='/register' element={user ? <Home /> : <Register />}>   
+      </Route>
+      <Route path='/login'  element={user ? <Home /> : <Login />}>
+      </Route>
+      <Route path='/settings' element={user ? <Settings /> : <Register />}>
+      </Route>
+      <Route path="/write" element={user ? <Write /> : <Register />}>
+      </Route>
+      <Route path='post/postId' element={<Single />}>
+      </Route>
+    </Routes>
+    </Router>
   );
 }
 
--- my-app/src/components/TopBar/TopBar.jsx
+++ my-app/src/components/TopBar/TopBar.jsx
@@ -1,25 +1,58 @@
 import "./TopBar.css"
+import { Link } from "react-router-dom"
 
 export default function TopBar() {
+    const user = false;
     return (
         <div className="top">
             <div className="topLeft">
                 <i className="topIcon fa-brands fa-facebook-square"></i>
-                <i className="topIcon fa-brands fa-twitter-square"></i>
+                <i className="topIcon fa-brands fa-twitter-square"></i>                               
                 <a href="https://www.instagram.com/mil_bixby/"><i className="topIcon fa-brands fa-instagram-square"></i></a>
                 <i className="topIcon fa-brands fa-youtube-square"></i>
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                   <li className="topListItem">HOME</li> 
-                   <li className="topListItem">ABOUT</li> 
-                   <li className="topListItem">CONTACT</li> 
-                   <li className="topListItem">WRITE</li> 
-                   <li className="topListItem">LOGOUT</li> 
+                   <li className="topListItem">
+                    <Link className="link" to="/">HOME</Link> 
+                   </li> 
+                   <li className="topListItem">
+                   <Link className="link" to="/">ABOUT</Link>
+                   </li> 
+                   <li className="topListItem">
+                   <Link className="link" to="/">CONTACT</Link>
+                   </li> 
+                   <li className="topListItem">
+                   <Link className="link" to="/write">WRITE</Link>
+                   </li> 
+                   <li className="topListItem">
+                   <Link className="link" to="/">
+                    {user && "LOGOUT"}
+                   </Link>
+                   </li> 
                 </ul>
             </div>
             <div className="topRight">
-                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i> 
+                {
+                    user ?  (
+                        <img
+                            className="topImg"
+                            src="https://i.postimg.cc/KjCWM5SX/35749773-10209126540959608-7432460838671220736-n.jpg"
+                            alt=""
+                />
+                    )   :   (
+                            <ul className="topList">
+                                <li className="topListItem">
+                                    <Link className="link" to="/login">LOGIN</Link>
+                                </li>
+                                <li className="topListItem">
+                                    <Link className="link" to="/register">REGISTER</Link>
+                                </li>    
+                            </ul>
+                    )
+                }
+                
+                <i className="topSearchIcon fas fa-search"></i> 
             </div>
         </div>
     )
