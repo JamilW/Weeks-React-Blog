cbab628712538aee2c09cfe62b6d769f07edef1d
Jamil Weeks <jamilweeks@yahoo.com>
2023-06-21 18:19:50 -0400

    worked on About Me sidebar

--- Weeks-React-Blog/my-app/src/components/sidebar/sidebar.jsx
+++ Weeks-React-Blog/my-app/src/components/sidebar/sidebar.jsx
@@ -6,7 +6,17 @@ export default function sidebar()   {
             <div className="sidebarItem">
                 <span className="sidebarTitle">ABOUT ME</span>
                 <img src="https://i.postimg.cc/X7WVcMwK/10995827-10202518527803409-4429824122052753281-n.jpg" alt=""></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>            
+                <p>Good day, fam! My name is Jamil. And I am a lover of music and movies. So much, that I wanted to share my love with all of you and create a community for healthy related discussions.
+                <br/>  
+                <br/>          
+                I created this blog for 80's babies who grew up on YO! MTV Raps and Rap City. Who copped The Purple Tape on cassette. Who waited in lines at the local record stores for midnight sales of their latest albums. Who love old school hip hop and appreciates the current hip hop climate.
+                <br/>  
+                <br/>   
+                In addition, this blog is for movies buffs. For those who watched their favorite movies from a VCR. Who were there opening night for the latest blockbuster. Who grew up on Siskel & Ebert film reviews. Who spent many Friday nights perusing the selections in Blockbuster Video.     
+                <br/>
+                <br/>
+                I look forward to building a community of respectful discourse and healthy debates, where hopefully we can learn from each other, and be entertained. This is my agenda......</p>
+            
             </div>
             <div className="sidebarItem">
                 <span className="sidebarTitle">CATEGORIES</span>
